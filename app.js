ac4b1b0332bdf7a13d8f7eab8d4c143db542f38a
nparekh13011 <55457971+nprkh13011@users.noreply.github.com>
2022-05-02 20:18:40 -0400

    css and other fun stuff i guess

--- app.js
+++ app.js
@@ -12,55 +12,55 @@ app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(express.urlencoded({extended: true}));
 
-// app.use(session({
-//   name: 'AuthCookie',
-//   secret: 'some secret string!',
-//   resave: false,
-//   saveUninitialized: true
-// }));
+app.use(session({
+  name: 'AuthCookie',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: true
+}));
 
-// // app.use('/', (req, res, next) => {
-// //   // console.log(req.session.id);
-// //   if (req.session.user) { // if they are authenticated
-// //     return res.redirect('/private');
-// //   } else {
-// //     next();
-// //   }
-// // });
-
-// app.use('/private', (req, res, next) => {
+// app.use('/', (req, res, next) => {
 //   // console.log(req.session.id);
-//   if (!req.session.user) {
-//     return res.redirect('/');
-//   } else {
-//     next();
-//   }
-// });
-// app.use('/signup', (req, res, next) => {
-//   // console.log(req.session.id);
-//   if (req.session.user) { // if user is authenticated
+//   if (req.session.user) { // if they are authenticated
 //     return res.redirect('/private');
 //   } else {
 //     next();
 //   }
 // });
 
-// app.use('/login', (req, res, next) => {
-//   if (req.session.user) {
-//     return res.redirect('/private');
-//   } else {
-//     //here I',m just manually setting the req.method to post since it's usually coming from a form
-//     req.method = 'POST';
-//     next();
-//   }
-// });
+app.use('/private', (req, res, next) => {
+  // console.log(req.session.id);
+  if (!req.session.user) {
+    return res.redirect('/');
+  } else {
+    next();
+  }
+});
+app.use('/signup', (req, res, next) => {
+  // console.log(req.session.id);
+  if (req.session.user) { // if user is authenticated
+    return res.redirect('/private');
+  } else {
+    next();
+  }
+});
 
-//   //logging middleware?
-//   app.use('*', (req, res, next) => {
-//     let auth = req.session.user ? "Authenticated User" : "Non-Authenticated User";
-//     console.log("[" + new Date().toUTCString() + "]: " + req.method + " " + req.originalUrl + ` ${auth}`);
-//     next();
-//   });
+app.use('/login', (req, res, next) => {
+  if (req.session.user) {
+    return res.redirect('/private');
+  } else {
+    //here I',m just manually setting the req.method to post since it's usually coming from a form
+    // req.method = 'POST';
+    next();
+  }
+});
+
+  //logging middleware?
+  app.use('*', (req, res, next) => {
+    let auth = req.session.user ? "Authenticated User" : "Non-Authenticated User";
+    console.log("[" + new Date().toUTCString() + "]: " + req.method + " " + req.originalUrl + ` ${auth}`);
+    next();
+  });
 
 configRoutes(app);
 
